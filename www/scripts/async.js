3f51c9a730130988f054151dac0d8c7f4e0d6bc2
Jesse Hattabaugh <jessehattabaugh@protonmail.com>
2025-03-04 10:58:07 -0800

    improving parcel install

--- www/scripts/async.js
+++ www/scripts/async.js
@@ -1,15 +1,1 @@
 // put JavaScript that doesn't require the DOM be fully parsed here
-
-// register a service worker to enable PWA
-if ('serviceWorker' in navigator) {
-	window.addEventListener('load', async () => {
-		try {
-			await navigator.serviceWorker.register(new URL('../sw.js', import.meta.url), {
-				scope: './',
-			});
-			console.debug('👨‍🏭® service worker registered');
-		} catch (exception) {
-			console.error('👨‍🏭⚠ service worker failed', exception);
-		}
-	});
-}
