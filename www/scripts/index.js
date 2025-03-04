0bfd196664a4ac6426a05886c1c95b62e78246af
Jesse Hattabaugh <jessehattabaugh@protonmail.com>
2025-03-04 14:45:38 -0800

    removing SPW routing nonsnense

--- www/scripts/index.js
+++
@@ -1,6 +0,0 @@
-// Import router which will handle page loading
-import { router } from './router.js';
-
-// Initialize the router
-const pageContainer = document.getElementById('page-container');
-router.init(pageContainer);
